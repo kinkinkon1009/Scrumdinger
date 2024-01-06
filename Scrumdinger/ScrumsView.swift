e0ed5652e28428fda17eb5878fe1912209ad04d8
kinkinkon1009 <ayumu.kanechika@gmail.com>
2024-01-06 21:39:40 +0900

    Displaying data in a list Section 1 Display a list of daily scrums
    https://developer.apple.com/tutorials/app-dev-training/displaying-data-in-a-list

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,23 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by 金近歩 on 2024/01/06.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums, id: \.title) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
